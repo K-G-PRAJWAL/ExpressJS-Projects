3f7c4d191d68b253eb3f39a9f6e95f80bb432466
K-G-PRAJWAL <prajuganesh1999@gmail.com>
2020-02-16 11:01:43 +0530

    Added Bitcoin Ticker Project

---
+++ BitcoinTicker/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+
+    var crypto = req.body.crypto;
+    var fiat = req.body.fiat;
+
+    var baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
+
+    var finalURL = baseURL + crypto + fiat;
+
+  request(
+    {
+      url: finalURL,
+      headers: {
+        "x-ba-key": process.env.API_KEY
+      }
+    },
+    function(error, response, body) {
+      var data = JSON.parse(body);
+      var price = data.last;
+      var currentDate = data.display_timestamp;
+      res.write("<p>The current date is: " + currentDate + "</p>");
+      res.write(
+        "<h1>The current price of bitcoin is : " +
+          price +
+          " " +
+          req.body.fiat +
+          "</h1>"
+      );
+      res.send();
+    }
+  );
+});
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000");
+});
