f2bd3e89aaeaede8c4a880293606311c5ab5c1be
K-G-PRAJWAL <prajuganesh1999@gmail.com>
2020-04-12 18:02:21 +0530

    Added Newsletter Signup App

---
+++ Newsletter-Signup/app.js
@@ -0,0 +1,63 @@
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  var options = {
+    url: "https://us4.api.mailchimp.com/3.0/lists/"+process.env.USER_ID,
+    method: "POST",
+    headers: {
+      Authorization: "Prajwal "+process.env.API_KEY
+    },
+    body: jsonData
+  };
+
+  request(options, function(error, response, body) {
+    if (error) {
+      res.sendFile(__dirname + "/failure.html");
+    } else {
+      if (response.statusCode === 200) {
+        res.sendFile(__dirname + "/success.html");
+      } else {
+        res.sendFile(__dirname + "/failure.html");
+      }
+    }
+  });
+});
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
+
+app.listen(3000, function() {
+  console.log("Server started at port 3000");
+});
